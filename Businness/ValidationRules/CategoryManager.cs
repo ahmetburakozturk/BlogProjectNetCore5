62b239769580aa90c87b168750562a5ecc8dd37a
Ahmet Burak Ozturk <79537376+ahmetburakozturk@users.noreply.github.com>
2022-02-27 04:43:41 +0300

    updateTwo

---
+++ Businness/ValidationRules/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Businness.Abstract;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+
+namespace Businness.ValidationRules
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
+
+        public void Add(Category entity)
+        {
+            efCategoryRepository.Add(entity);
+        }
+
+        public void Delete(Category entity)
+        {
+            efCategoryRepository.Delete(entity);
+        }
+
+        public Category Get(int id)
+        {
+            return efCategoryRepository.Get(id);
+        }
+
+        public List<Category> GetAll()
+        {
+            return efCategoryRepository.GetAll();
+        }
+
+        public void Update(Category entity)
+        {
+            efCategoryRepository.Update(entity);
+        }
+    }
+}
---
+++ DataAccess/EntityFramework/EfCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+using DataAccess.Repositories;
+using Entities.Concrete;
+
+namespace DataAccess.EntityFramework
+{
+    public class EfCategoryRepository : GenericRepository<Category>, ICategoryDal
+    {
+
+    }
+}
